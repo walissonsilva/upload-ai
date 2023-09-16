2af7c29ad1384062ba7b4525b5b84216b3303886
Walisson Silva <walissonsilva10@gmail.com>
2023-09-16 00:06:14 -0300

    feat(back): create routes to get prompts and upload video

---
+++ back/src/routes/upload-video.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
+import path from "node:path";
+import { randomUUID } from "node:crypto";
+import fs from "fs";
+import { promisify } from "node:util";
+import { pipeline } from "node:stream";
+
+const pump = promisify(pipeline);
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      files: 1_048_576 * 25, // 25 MB
+    },
+  });
+
+  app.post("/videos", async (request, response) => {
+    const data = await request.file();
+
+    if (!data) {
+      return response.status(400).send({ error: "Missing file input" });
+    }
+
+    const extension = path.extname(data.filename);
+
+    if (extension !== ".mp3") {
+      return response
+        .status(400)
+        .send({ error: "Invalid input type, please upload a mp3." });
+    }
+
+    const fileBaseName = path.basename(data.filename, extension);
+
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`;
+
+    const uploadDir = path.resolve(__dirname, "../../tmp", fileUploadName);
+
+    await pump(data.file, fs.createWriteStream(uploadDir));
+  });
+}
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,11 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello World!";
-});
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
 app
   .listen({
