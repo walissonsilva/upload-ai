054c80bf33e76e05b2d13623e35a604c432dab32
Walisson Silva <walissonsilva10@gmail.com>
2023-09-17 15:43:35 -0300

    feat(front): handle upload form submission and convert video using ffmpeg with webassembly

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import { Button } from "./components/ui/button";
-import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
+import { Github, Wand2 } from "lucide-react";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
@@ -12,6 +12,7 @@ import {
   SelectValue,
 } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
 
 function App() {
   return (
@@ -54,39 +55,7 @@ function App() {
         </section>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex rounded-md aspect-video justify-center items-center cursor-pointer border-dashed text-sm flex-col gap-2 text-muted-foreground hover:bg-primary/10"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um vídeo
-            </label>
-            <input
-              type="file"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por ','"
-              />
-            </div>
-
-            <Button type="submit" className="w-full" disabled>
-              Carregar vídeo <Upload className="w-4  h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
---
+++ front/src/components/video-input-form.tsx
@@ -0,0 +1,132 @@
+import { FileVideo, Upload } from "lucide-react";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { Label } from "./ui/label";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
+
+export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const promptInputRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget;
+
+    if (!files) {
+      return;
+    }
+
+    const selectedFile = files[0];
+    setVideoFile(selectedFile);
+  }
+
+  async function convertVideoToAudio(video: File) {
+    console.log("Convertion started...");
+
+    const ffmpeg = await getFFmpeg();
+
+    await ffmpeg.writeFile("input.mp4", await fetchFile(video));
+
+    // ffmpeg.on("log", (log) => {
+    //   console.log(log);
+    // });
+
+    ffmpeg.on("progress", (progress) => {
+      console.log("Convert progress: ", Math.round(progress.progress * 100));
+    });
+
+    await ffmpeg.exec([
+      "-i",
+      "input.mp4",
+      "-map",
+      "0:a",
+      "-b:a",
+      "20k",
+      "-acodec",
+      "libmp3lame",
+      "output.mp3",
+    ]);
+
+    const data = await ffmpeg.readFile("output.mp3");
+
+    const audioFileBlob = new Blob([data], { type: "audio/mpeg" });
+    const audioFile = new File([audioFileBlob], "audio.mp3", {
+      type: "audio/mpeg",
+    });
+
+    console.log("Conversion finished.");
+
+    return audioFile;
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const prompt = promptInputRef.current?.value;
+
+    if (!videoFile) return;
+
+    // converter vídeo em áudio
+    const audioFile = await convertVideoToAudio(videoFile);
+
+    console.log(audioFile, prompt);
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) return null;
+
+    return URL.createObjectURL(videoFile);
+  }, [videoFile]);
+
+  return (
+    <form onSubmit={handleUploadVideo} className="space-y-6">
+      <label
+        htmlFor="video"
+        className="relative border flex rounded-md aspect-video justify-center items-center cursor-pointer border-dashed text-sm flex-col gap-2 text-muted-foreground hover:bg-primary/10"
+      >
+        {previewURL ? (
+          <video
+            src={previewURL}
+            controls={false}
+            className="absolute pointer-events-none inset-0"
+          ></video>
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione um vídeo
+          </>
+        )}
+      </label>
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
+
+      <Separator />
+
+      <div className="space-y-2">
+        <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+
+        <Textarea
+          id="transcription_prompt"
+          className="h-20 leading-relaxed resize-none"
+          placeholder="Inclua palavras-chave mencionadas no vídeo separadas por ','"
+          ref={promptInputRef}
+        />
+      </div>
+
+      <Button
+        type="submit"
+        className="w-full"
+        disabled={!previewURL || promptInputRef == null}
+      >
+        Carregar vídeo <Upload className="w-4  h-4 ml-2" />
+      </Button>
+    </form>
+  );
+}
