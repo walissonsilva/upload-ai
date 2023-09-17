64e40224dbaa928057833b1f5aacf5852026d3c3
Walisson Silva <walissonsilva10@gmail.com>
2023-09-17 00:21:43 -0300

    feat(back): create route to generate video description based on video transcription using ChatGPT

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,12 +2,14 @@ import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app
   .listen({
